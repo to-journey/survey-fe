3dbd90d73fa9315c71ddf3e37d5313d6fe08b858
to-journey <180360578+to-journey@users.noreply.github.com>
2025-05-12 23:44:41 +0900

    feat: impl user survey detail page

--- src/routes/user/survey/$id.tsx
+++ src/routes/user/survey/$id.tsx
@@ -1,9 +1,19 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { Container } from '@chakra-ui/react'
+import { createFileRoute, useParams } from '@tanstack/react-router'
+import useSurvey from '@/hooks/useSurvey'
+import SurveyDetail from '@/components/survey/SurveyDetail'
 
 export const Route = createFileRoute('/user/survey/$id')({
   component: RouteComponent,
 })
 
 function RouteComponent() {
-  return <div>Hello "/user/survey/$id"!</div>
+  const { id } = useParams({ from: '/user/survey/$id' })
+  const { survey } = useSurvey(id)
+  
+  return (
+    <Container maxW="container.xl" py={10}>
+      {survey && <SurveyDetail survey={survey} />}
+    </Container>
+  )
 }
